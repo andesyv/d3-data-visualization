ba25573ede979cdfe121b68da00726e5156e974c
Anders Syvertsen <andesyv@gmail.com>
2020-09-15 16:31:16 +0200

    Refactored code into multiple files
    Alot easier to read... Does however require you to use a web-server, tho. But no fancy web-server.

---
+++ js/chart1.js
@@ -0,0 +1,57 @@
+const valueCount = 10,
+  width = 600,
+  height = 500,
+  margin = 30;
+
+export default (dataset) => {
+  var canvas = d3
+    .select("body")
+    .append("svg")
+    .attr("width", 600)
+    .attr("height", 500);
+
+  let x_scale = d3
+    .scaleBand()
+    .domain(dataset.map((d) => d.date))
+    .range([margin, 500]);
+  let y_scale = d3
+    .scaleLinear()
+    .domain([
+      d3.min(dataset, (d) => d.avgtmp),
+      d3.max(dataset, (d) => d.avgtmp),
+    ])
+    .range([margin, 450]);
+
+  let bars = canvas
+    .selectAll(".bar")
+    .data(dataset)
+    .enter()
+    .append("g")
+    .attr("transform", (d) => {
+      return `translate(${x_scale(d.date)}, ${y_scale(d.avgtmp)})`;
+    });
+  bars
+    .append("rect")
+    .attr("x", "0")
+    .attr("y", /*`${canvas.attr("height")}`*/ "0")
+    .attr("width", `${x_scale.bandwidth()}`)
+    .attr("height", (d) => {
+      return canvas.attr("height") - y_scale(d.avgtmp) - margin;
+    });
+  bars
+    .append("text")
+    .attr("x", `${x_scale.bandwidth() * 0.133}`)
+    .attr("y", "-.35em")
+    .text((d) => {
+      return d.avgtmp;
+    });
+
+  let x_axis = canvas
+    .append("g")
+    .call(d3.axisBottom(x_scale).tickFormat(d3.timeFormat("%d/%m")))
+    .attr("transform", `translate(0, ${canvas.attr("height") - margin})`);
+  let y_axis = canvas
+    .append("g")
+    .call(d3.axisLeft(y_scale))
+    .attr("transform", `translate(${margin}, 0)`);
+};
--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+import chart1 from "./chart1.js";
+import chart2 from "./chart2.js";
+
 function cleanseVal(val) {
   return val == "---" ? null : val;
 }
@@ -45,111 +48,16 @@ d3.csv(
   }
 )
   .then((dataset) => {
-    var canvas = d3
-      .select("body")
-      .append("svg")
-      .attr("width", 600)
-      .attr("height", 500);
-
-    let x_scale = d3
-      .scaleBand()
-      .domain(dataset.map((d) => d.date))
-      .range([margin, 500]);
-    let y_scale = d3
-      .scaleLinear()
-      .domain([
-        d3.min(dataset, (d) => d.avgtmp),
-        d3.max(dataset, (d) => d.avgtmp),
-      ])
-      .range([margin, 450]);
-
-    let bars = canvas
-      .selectAll(".bar")
-      .data(dataset)
-      .enter()
-      .append("g")
-      .attr("transform", (d) => {
-        return `translate(${x_scale(d.date)}, ${y_scale(d.avgtmp)})`;
-      });
-    bars
-      .append("rect")
-      .attr("x", "0")
-      .attr("y", /*`${canvas.attr("height")}`*/ "0")
-      .attr("width", `${x_scale.bandwidth()}`)
-      .attr("height", (d) => {
-        return canvas.attr("height") - y_scale(d.avgtmp) - margin;
-      });
-    bars
-      .append("text")
-      .attr("x", `${x_scale.bandwidth() * 0.133}`)
-      .attr("y", "-.35em")
-      .text((d) => {
-        return d.avgtmp;
-      });
-
-    let x_axis = canvas
-      .append("g")
-      .call(d3.axisBottom(x_scale).tickFormat(d3.timeFormat("%d/%m")))
-      .attr("transform", `translate(0, ${canvas.attr("height") - margin})`);
-    let y_axis = canvas
-      .append("g")
-      .call(d3.axisLeft(y_scale))
-      .attr("transform", `translate(${margin}, 0)`);
-
-    // Chart 2
-    let series = d3
-      .stack()
-      .keys(["mintmp", "avgtmp", "maxtmp"])
-      .order(d3.stackOrderNone)
-      .offset(d3.stackOffsetNone)(dataset);
-
-    canvas = d3
-      .select("body")
-      .append("svg")
-      .attr("width", 600)
-      .attr("height", 500);
-    x_scale = d3
-      .scaleBand()
-      .domain(dataset.map((d) => d.date))
-      .range([margin, 500]);
-    // Create a new band scale with the same output range of x_scale but the domain of the indexes in the series
-    let color_scale = d3
-      .scaleOrdinal()
-      .domain(series.map((v, i) => i))
-      .range(d3.schemeCategory10);
-    y_scale = d3
-      .scaleLinear()
-      .domain([
-        0,
-        d3.max(dataset, (d) => {
-          return d.mintmp + d.avgtmp + d.maxtmp;
-        }),
-      ])
-      .range([450, margin]); // Reversing range (up on y-axis means larger)
-
-    x_axis = canvas
-      .append("g")
-      .call(d3.axisBottom(x_scale).tickFormat(d3.timeFormat("%d/%m")))
-      .attr("transform", `translate(0, ${canvas.attr("height") - margin})`);
-    y_axis = canvas
-      .append("g")
-      .call(d3.axisLeft(y_scale))
-      .attr("transform", `translate(${margin}, 0)`);
-
-    let area = d3
-      .area()
-      .x((d) => x_scale(d.data.date))
-      .y0((d) => y_scale(d[0]))
-      .y1((d) => y_scale(d[1]));
-
-    let path = canvas
-      .selectAll()
-      .data(series)
-      .join("path")
-      .attr("fill", ({ key }) => color_scale(key))
-      .attr("stroke", "steelblue")
-      .attr("stroke-width", 1.5)
-      .attr("d", area);
+    try {
+      chart1(dataset);
+    } catch (err) {
+      console.log(`Error chart1: ${err}`);
+    }
+    try {
+      chart2(dataset);
+    } catch (err) {
+      console.log(`Error chart2: ${err}`);
+    }
   })
   .catch((err) => {
     console.log(`Failed to load data: ${err}`);
