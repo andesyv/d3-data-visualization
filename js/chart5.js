488e871c3eb44105cd04a946f1759542a710ee1d
Anders Syvertsen <andesyv@gmail.com>
2020-09-20 16:16:16 +0200

    Amount of rain per month in a slightly less mess

--- js/chart5.js
+++ js/chart5.js
@@ -4,23 +4,41 @@ const valueCount = 10,
   margin = 30;
 
 function removeYear(date) {
-  return d3.timeParse("%B %d, %Y")(d3.timeFormat("%B %d")(date).concat(", 2020"));
-};
+  return d3.timeParse("%B %d, %Y")(
+    d3.timeFormat("%B %d")(date).concat(", 2020")
+  );
+}
+
+function avgReduction(acc, val, index) {
+  return index ? (acc * (index - 1) + val) / index : acc;
+}
 
 export default (dataset) => {
   // dataset = dataset.slice(0, 10);
   const data = d3
-    .groups(dataset.filter(dinner => dinner.date && dinner.precip), (d) => d.year)
+    .groups(
+      dataset.filter((dinner) => dinner.date && dinner.precip),
+      (d) => d.year
+    )
     .sort((l, r) => {
       return l[0] < r[0];
     })
-    .map((d) => d[1]);
+    .map((d) => d[1])
+    .map((y) => {
+      let months = [];
+      return Array(12)
+        .fill(null)
+        .map((v, i) => {
+          let arr = y.filter((d) => d.month === i + 1);
+          return arr.length
+            ? arr
+                .map((d) => d.precip)
+                .reduce((acc, val, index) => (index ? acc + val : acc))
+            : null;
+        });
+    });
+
   console.log(data);
-  // let series = d3
-  //   .stack()
-  //   .keys(["mintmp", "avgtmp", "maxtmp"])
-  //   .order(d3.stackOrderNone)
-  //   .offset(d3.stackOffsetNone)(dataset);
 
   let canvas = d3
     .select("body")
@@ -28,24 +46,21 @@ export default (dataset) => {
     .attr("width", 600)
     .attr("height", 500);
 
-  let x_scale = d3
-    .scaleTime()
-    .domain([new Date(2020, 0, 1), new Date(2020, 11, 31)])
-    .range([margin, 500]);
+  let x_scale = d3.scaleLinear().domain([0, 11]).range([margin, 500]);
 
   let color_scale = d3
     .scaleOrdinal()
-    .domain(data.map(d => d[0].year))
+    .domain(data.map((d, i) => i))
     .range(d3.schemeCategory10);
 
   let y_scale = d3
     .scaleLinear()
-    .domain([0, d3.max(dataset, (d) => d.precip)])
+    .domain([0, d3.max(data.map((dinner) => d3.max(dinner)))])
     .range([450, margin]); // Reversing range (up on y-axis means larger)
 
   let x_axis = canvas
     .append("g")
-    .call(d3.axisBottom(x_scale).tickFormat(d3.timeFormat("%d/%m")))
+    .call(d3.axisBottom(x_scale).tickFormat((d) => d + 1))
     .attr("transform", `translate(0, ${canvas.attr("height") - margin})`);
 
   let y_axis = canvas
@@ -55,15 +70,15 @@ export default (dataset) => {
 
   let line = d3
     .line()
-    .x((d) => x_scale(removeYear(d.date)))
-    .y((d) => y_scale(d.precip));
+    .x((d, i) => x_scale(i))
+    .y((d) => y_scale(d));
 
   let path = canvas
     .selectAll()
     .data(data)
     .join("path")
     .attr("fill", "none")
-    .attr("stroke", (d) => color_scale(d[0].year))
+    .attr("stroke", (d, i) => color_scale(i))
     .attr("stroke-width", 1.5)
     .attr("d", line);
 };
